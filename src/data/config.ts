7307c9f558d36dd2c9b4d4a81a19f4663caf91f6
Ashlxxy <ashlxxy@users.noreply.github.com>
2026-05-12 23:08:24 +0700

    Initial portfolio website

---
+++ src/data/config.ts
@@ -0,0 +1,47 @@
+const config = {
+  title: "Ade Rainhard Pasaribu | Full-Stack Developer",
+  description: {
+    long: "Explore the portfolio of Ade Rainhard Pasaribu, a full-stack developer focused on interactive web experiences, 3D animations, and modern digital projects. Let's build something amazing together!",
+    short:
+      "Discover the portfolio of Ade Rainhard Pasaribu, a full-stack developer creating interactive web experiences and modern digital projects.",
+  },
+  keywords: [
+    "Ade Rainhard Pasaribu",
+    "Ade Rainhard",
+    "Ashlxxy",
+    "portfolio",
+    "full-stack developer",
+    "creative technologist",
+    "web development",
+    "3D animations",
+    "interactive websites",
+    "Coding Ducks",
+    "The Booking Desk",
+    "Ghostchat",
+    "web design",
+    "GSAP",
+    "React",
+    "Next.js",
+    "Spline",
+    "Framer Motion",
+  ],
+  author: "Ade Rainhard Pasaribu",
+  email: "aderainhardp@gmail.com",
+  site: "https://github.com/Ashlxxy",
+
+  // for github stars button
+  githubUsername: "Ashlxxy",
+  githubRepo: "",
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    twitter: "https://x.com/Ashlxy_el",
+    linkedin: "https://www.linkedin.com/in/ade-rainhard-pasaribu-42426432a",
+    instagram: "https://www.instagram.com/aderainhard",
+    facebook: "",
+    github: "https://github.com/Ashlxxy",
+  },
+};
+export { config };
